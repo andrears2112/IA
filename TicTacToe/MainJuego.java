8e02903b1158e96d42c51434c0505945f1d28506
Andrea Rios Sicairos <127136581+andrears2112@users.noreply.github.com>
2023-11-25 20:37:59 -0700

    Update MainJuego.java

--- TicTacToe/MainJuego.java
+++ TicTacToe/MainJuego.java
@@ -1,9 +1,31 @@
+/*
+ 	INTEGRANTES:
+ 		-Rios Sicairos Andrea
+ 		-Villegas Solis Jose Manuel
+*/
+
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class MainJuego {
+	
 
 	public static void main(String[] args) {
-		TicTacToe ticTacToe = new TicTacToe();
-        ticTacToe.start();
+		Scanner Leer = new Scanner(System.in);
+		int jugadorInicia=0;
+		
+		while(true) {
+			System.out.println("TicTacToe \n\nSeleccione quien empezara:\n 1:Bot\n 2:Persona\n 3:Salir");
+			jugadorInicia = Leer.nextInt();
+			if(jugadorInicia==3){System.out.println("Finalizado");break;}
+			else if(jugadorInicia==1 || jugadorInicia==2){
+				TicTacToe ticTacToe = new TicTacToe();
+		        ticTacToe.start(jugadorInicia);
+			}else {
+				System.out.println("Elija una de las opciones del Menu:");
+			}
+		}
+	
 	}
 }
